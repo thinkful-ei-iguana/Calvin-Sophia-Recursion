921ce71a230490e502d5eba1eb3e57324e1f7f79
Sophia Koeut <47515086+cantuus@users.noreply.github.com>
2020-01-21 13:21:16 -0800

    fibonacci works!

--- calvin-sophia-recursion/src/recursion-drills.js
+++ calvin-sophia-recursion/src/recursion-drills.js
@@ -4,6 +4,20 @@
 // Write a recursive function that splits a string based on a separator (similar to String.prototype.split).
 // Don't use JS array's split function to solve this problem.
 
+//Fibonacci Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. 
+//The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
+
+const fibo = function (num) {
+  if(num < 2) {
+    return num;
+  }
+
+  return fibo(num - 1) + fibo(num - 2);
+}
+
+console.log(fibo(7));
+
+
 const stringSplit = function(strArr, separator) {
   if (strArr.length === 1) {
     console.log("Base case");
