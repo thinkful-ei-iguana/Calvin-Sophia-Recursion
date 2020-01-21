13b6a27c2cf266fe8d6689ef1b7522d376defd51
Sophia Koeut <47515086+cantuus@users.noreply.github.com>
2020-01-21 14:04:49 -0800

    working on maze drill

--- calvin-sophia-recursion/src/recursion-drills.js
+++ calvin-sophia-recursion/src/recursion-drills.js
@@ -7,6 +7,19 @@
 //Fibonacci Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. 
 //The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
 
+const exitMaze = function ( ) {
+  //currentPosition and pastposition
+  //separator asterisk
+  //parameters: position, blocker
+  //be aware of adjacent squares
+  //array of adjacent squares
+  //let adjs = []
+
+}
+
+exitMaze() 
+
+
 const factorial = function (num) {
   if( num == 0) {
     return 1
