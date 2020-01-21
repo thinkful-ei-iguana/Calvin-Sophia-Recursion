99d0e688fb1ef47c72101ed7aedd1cdecac11147
Sophia Koeut <47515086+cantuus@users.noreply.github.com>
2020-01-21 13:40:32 -0800

    factorial works!

--- calvin-sophia-recursion/src/recursion-drills.js
+++ calvin-sophia-recursion/src/recursion-drills.js
@@ -7,6 +7,16 @@
 //Fibonacci Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. 
 //The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
 
+const factorial = function (num) {
+  if( num == 0) {
+    return 1
+  }
+
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(7));
+
 const fibo = function (num) {
   if(num < 2) {
     return num;
